c117100e23f758eba9eb946da059f2472db7867b
Farhan Sifar <sifarf@mcmaster.ca>
2024-11-25 19:26:58 -0500

    Iteration 1B completed

--- Project.cpp
+++ Project.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include "MacUILib.h"
 #include "objPos.h"
+#include "GameMechs.h"
 
 using namespace std;
 
 #define DELAY_CONST 100000
 #define row 10
 #define size 20
+
 
 bool exitFlag;
 
@@ -17,13 +19,13 @@ void DrawScreen(void);
 void LoopDelay(void);
 void CleanUp(void);
 
-
+GameMechs* gamemechs = nullptr;
 
 int main(void)
 {
     Initialize();
 
-    while(exitFlag == false)  
+    while(!gamemechs -> getExitFlagStatus())  
     {
         GetInput();
         RunLogic();
@@ -41,16 +43,33 @@ void Initialize(void)
     MacUILib_clearScreen();
 
     exitFlag = false;
+    gamemechs = new GameMechs();
 }
 
 void GetInput(void)
 {
-   
+    char input = MacUILib_getChar();
+   gamemechs -> setInput(input);
 }
 
 void RunLogic(void)
 {
+    char input = gamemechs -> getInput();
+    if (input == 27){
+        gamemechs -> setExitTrue();
+    }
+    else if (input == 'x'){
+        gamemechs -> incrementScore();
+        MacUILib_printf("the score is %d\n", gamemechs -> getScore());
+    }
+    else if (input == 'z'){
+        gamemechs -> setLoseFlag();
+        MacUILib_printf("exiting the game");
+        gamemechs -> setExitTrue();
+    }
     
+    gamemechs -> clearInput();
+
 }
 
 void DrawScreen(void)
@@ -86,6 +105,9 @@ void LoopDelay(void)
 void CleanUp(void)
 {
     MacUILib_clearScreen();    
+
+    delete gamemechs;
+    gamemechs = nullptr;
 
     MacUILib_uninit();
 }
