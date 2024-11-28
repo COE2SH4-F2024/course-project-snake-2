ef1367335fd966d047569450e63e5950f14ca563
wonge84 <wonge84@mcmaster.ca>
2024-11-27 23:31:29 -0500

    C

--- GameMechs.h
+++ GameMechs.h
@@ -25,7 +25,7 @@ class GameMechs
     public:
         GameMechs();
         GameMechs(int boardX, int boardY);
-        ~GameMechs(); // is this one needed at all? Why or why not?
+        ~GameMechs();
         
         bool getExitFlagStatus() const; 
         void setExitTrue();
@@ -43,8 +43,6 @@ class GameMechs
         void setScore(int newScore);
         void incrementScore();
         void decrementScore();
-        
-        // More methods should be added here
 };
 
 #endif
